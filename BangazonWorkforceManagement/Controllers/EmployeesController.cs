25b8a6e5b3be98a7e1928fef487f61d2b9626553
ollieOsh <sosinusi@gmail.com>
2017-08-24 14:19:28 -0500

    IT WOOOORRRRKKSSSS

--- BangazonWorkforceManagement/Controllers/EmployeesController.cs
+++ BangazonWorkforceManagement/Controllers/EmployeesController.cs
@@ -51,17 +51,42 @@ namespace BangazonWorkforceManagement.Controllers
                 var empComputer = _context.Computer.SingleOrDefault(c => c.ComputerId == item.ComputerId);
                 employeeView.ComputerList.Add(empComputer);
             }
-            //foreach (var item in employee.TrainingPgmEmps)
-            //{
+            foreach (var item in employee.TrainingPgmEmps)
+            {
                 //var empTrainingPgm = _context.TrainingProgram.Include("TrainingPgmEmp").Where(t => t.TrainingProgramEmps. && t.StartDate > DateTime.Now).DefaultIfEmpty(new TrainingProgram() { Name = "No future training program" }).Single();
                 //employeeView.FuturePrograms.Add(empTrainingPgm);
 
-                
+                var fp = from tp in _context.TrainingPgmEmp
+                         join t in _context.TrainingProgram
+                         on tp.TrainingProgramId equals t.TrainingProgramId
+                         where t.StartDate > DateTime.Now && tp.EmployeeId == id
+                         select tp.TrainingProgram;
 
-                employeeView.AttendedPrograms = _context.TrainingProgram.Select(t => t).Where(t => t.TrainingProgramId == item.TrainingProgramId && item.EmployeeId == id && t.StartDate <= DateTime.Now).ToList();
+                employeeView.FuturePrograms = fp.ToList();
 
-                employeeView.NotAttendingPrograms = _context.TrainingProgram.Select(t => t).Where(t => t.TrainingProgramId != item.TrainingProgramId && item.EmployeeId == id && t.StartDate > DateTime.Now).ToList();
-            //}
+                var ap = from tp in _context.TrainingPgmEmp
+                         join t in _context.TrainingProgram
+                         on tp.TrainingProgramId equals t.TrainingProgramId
+                         where t.StartDate < DateTime.Now && tp.EmployeeId == id
+                         select tp.TrainingProgram;
+
+                employeeView.AttendedPrograms = ap.ToList();
+            }
+
+            foreach(var item in _context.TrainingProgram)
+            {
+                var nap = from t in _context.TrainingProgram
+                          where t.StartDate > DateTime.Now
+                          select t;
+                var napList = nap.ToList();
+
+                foreach (var pgm in employeeView.FuturePrograms)
+                {
+                    napList.Remove(pgm);
+                }
+
+                employeeView.NotAttendingPrograms = napList;
+            }
 
             return View(employeeView);
         }
