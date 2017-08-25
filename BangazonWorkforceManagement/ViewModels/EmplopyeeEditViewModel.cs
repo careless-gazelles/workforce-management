703e31082f3f9acceb744c3b9db9ba57b9ebae9e
krissycaron <krissycaron@gmail.com>
2017-08-25 10:02:32 -0500

    EMpComp now postong new instance to DB

---
+++ BangazonWorkforceManagement/ViewModels/EmplopyeeEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforceManagement.Models.ViewModels
+{
+    public class EmployeeEditViewModel
+    {
+        public List<Computer> Computers { get; set; }
+        public int ComputerId { get; set; }
+        public Employee Employee { get; set; }
+        public EmployeeComputer EmployeeComputer { get; set; }
+
+    }
+}
